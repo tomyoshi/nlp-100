5f5aa93fd520c08ec944f39abb006b7259079f3b
ty444 <tomyoshi1995@gmail.com>
2020-09-23 07:45:23 +0900

    solve 00 and add testFile

---
+++ section2/00.py
@@ -0,0 +1,8 @@
+
+print(len(open('./testFile/popular-name.txt').readlines()))
+
+# 2780
+# python3 00.py  0.02s user 0.01s system 89% cpu 0.038 total
+
+
+# wc -l 00.py  0.00s user 0.00s system 49% cpu 0.011 total
