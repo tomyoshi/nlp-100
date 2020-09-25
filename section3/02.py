090a9d0b1cb257b6cf10f169b4a0f4623baebb47
ty444 <tomyoshi1995@gmail.com>
2020-09-25 12:47:02 +0900

    solve 02

---
+++ section3/02.py
@@ -0,0 +1,15 @@
+import json
+import re
+
+pattern = r'^.*\[\[Category:(.*)\]\].*$'
+
+with open("./testFile/jawiki-country.json", "r") as f:
+
+    for line in f:
+        line = json.loads(line)
+        if line['title'] == 'イギリス':
+            text = line['text']
+
+
+result = '\n'.join(re.findall(pattern, text, re.MULTILINE))
+print(result)
