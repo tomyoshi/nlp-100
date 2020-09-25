f5f2f93296a3a4282a8ff6420cc43c4f678f1f69
ty444 <tomyoshi1995@gmail.com>
2020-09-25 12:18:03 +0900

    solve 00

--- section3/00.py
+++ section3/00.py
@@ -1,1 +1,10 @@
-# none
+import json
+
+
+with open("./testFile/jawiki-country.json", encoding='utf-8') as f:
+
+    for line in f:
+        line = json.loads(line)
+
+        if line['title'] == 'イギリス':
+            print(line['text'])
